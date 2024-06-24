7f2cb9cdc6850bf650a113b8a8e62ff3d6d468ef
Abdul Haseeb <143028440+ahaseeb16@users.noreply.github.com>
2024-06-24 19:04:12 +0500

    list and maps

--- question_no1.dart
+++ question_no1.dart
@@ -1,5 +1,4 @@
-// Q.1: Create a list of names and print all names using list.
-//
+// Q.1: Create a list of names and print all names using list. //
 
 void main() {
   List names = ["Anas", "Ali", "Hasham", "Bilal", "Umer"];
